184583a42895cf29ff2752de32c1daab01949abc
Syedz68 <syedasheq@gmail.com>
2026-03-05 11:35:41 +0600

    Initial enums

---
+++ app/utils/enums.py
@@ -0,0 +1,30 @@
+import enum
+
+class UserRole(str, enum.Enum):
+    admin = "admin"
+    analyst = "analyst"
+    viewer = "viewer"
+
+
+class Permission(str, enum.Enum):
+    SELECT_ONLY = "SELECT_ONLY"
+    READ_WRITE = "READ_WRITE"
+    ADMIN_ACCESS = "ADMIN_ACCESS"
+
+
+class DataBaseType(str, enum.Enum):
+    postgres = "postgres"
+    mysql = "mysql"
+    sqlite = "sqlite"
+    oracle = "oracle"
+    sql_server = "sql_server"
+
+
+class ChatRole(str, enum.Enum):
+    user = "user"
+    system = "system"
+
+
+class LlmMode(str, enum.Enum):
+    local = "local"
+    online = "online"
